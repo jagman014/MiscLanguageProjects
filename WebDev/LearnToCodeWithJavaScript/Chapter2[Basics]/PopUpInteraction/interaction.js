c9adcd79955bf4708343601db79cec0c0a213636
Jag <72930861+jagman014@users.noreply.github.com>
2022-07-05 18:21:39 +0100

    finished chapter 2 about coding basics

---
+++ WebDev/LearnToCodeWithJavaScript/Chapter2[Basics]/PopUpInteraction/interaction.js
@@ -0,0 +1,18 @@
+// Alter box
+// user needs to click button to remove
+alert('hello');
+
+const message = 'Hello There!';
+alert(message);
+
+// Prompt Box
+// allows user to submit a response, that then can be stored
+
+const name = prompt('What is your name?');
+console.log('Name entered:', name);
+
+// Confirm Box
+// allows user to confirm or deny a request
+
+const choice = confirm('True or False?');
+console.log('Choice value:', choice);
---
+++ WebDev/LearnToCodeWithJavaScript/Chapter2[Basics]/basics.js
@@ -0,0 +1,26 @@
+// standard comments
+
+/*
+ * Multiline comments
+ */
+
+// similar to C/C++ end lines with ';' and code blocks in {}
+
+{
+    // This is a code block, with a comment and two statements
+    const name = prompt('Please enter your name.');
+    alert(name);
+}
+
+// whitespace is basically ignored in js
+
+// all primitive types:
+// strings, numbers, bools, symbol, bigint, undefined
+
+// const sets variable to constant
+// let allows mutability
+// var is generic catch all
+
+// undefined is what it says it is
+
+// variable names can start with $, _, or [a-zA-Z]
