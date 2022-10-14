4a8e025d124698feb86520abbf1c599b61e09938
Jag <72930861+jagman014@users.noreply.github.com>
2022-10-14 16:40:48 +0100

    update completed files for wsl postgresql server

--- SQL/PracticalSQL/Chapter_02/Chapter_02.sql
+++ SQL/PracticalSQL/Chapter_02/Chapter_02.sql
@@ -7,11 +7,16 @@
 
 -- Listing 2-1: Querying all rows and columns from the teachers table
 
-SELECT * FROM teachers;
+SELECT * 
+FROM teachers;
 
 -- Listing 2-2: Querying a subset of columns
 
-SELECT last_name, first_name, salary FROM teachers;
+SELECT 
+    last_name
+    ,first_name
+    ,salary
+FROM teachers;
 
 -- Listing 2-3: Querying distinct values in the school column
 
@@ -21,31 +26,47 @@ FROM teachers;
 -- Listing 2-4: Querying distinct pairs of values in the school and salary
 -- columns
 
-SELECT DISTINCT school, salary
+SELECT DISTINCT 
+    school
+    ,salary
 FROM teachers;
 
 -- Listing 2-5: Sorting a column with ORDER BY
 
-SELECT first_name, last_name, salary
+SELECT 
+    first_name
+    ,last_name
+    ,salary
 FROM teachers
 ORDER BY salary DESC;
 
 -- Listing 2-6: Sorting multiple columns with ORDER BY
 
-SELECT last_name, school, hire_date
+SELECT 
+    last_name
+    ,school
+    ,hire_date
 FROM teachers
-ORDER BY school ASC, hire_date DESC;
+ORDER BY 
+    school ASC
+    ,hire_date DESC;
 
 -- Listing 2-7: Filtering rows using WHERE
 
-SELECT last_name, school, hire_date
+SELECT 
+    last_name
+    ,school
+    ,hire_date
 FROM teachers
 WHERE school = 'Myers Middle School';
 
 -- Examples of WHERE comparison operators
 
 -- Teachers with first name of Janet
-SELECT first_name, last_name, school
+SELECT 
+    first_name
+    ,last_name
+    ,school
 FROM teachers
 WHERE first_name = 'Janet';
 
@@ -55,17 +76,27 @@ FROM teachers
 WHERE school != 'F.D. Roosevelt HS';
 
 -- Teachers hired before Jan. 1, 2000
-SELECT first_name, last_name, hire_date
+SELECT 
+    first_name
+    ,last_name
+    ,hire_date
 FROM teachers
 WHERE hire_date < '2000-01-01';
 
 -- Teachers earning 43,500 or more
-SELECT first_name, last_name, salary
+SELECT 
+    first_name
+    ,last_name
+    ,salary
 FROM teachers
 WHERE salary >= 43500;
 
 -- Teachers who earn between $40,000 and $65,000
-SELECT first_name, last_name, school, salary
+SELECT 
+    first_name
+    ,last_name
+    ,school
+    ,salary
 FROM teachers
 WHERE salary BETWEEN 40000 AND 65000;
 
@@ -84,21 +115,29 @@ WHERE first_name ILIKE 'sam%';
 SELECT *
 FROM teachers
 WHERE school = 'Myers Middle School'
-      AND salary < 40000;
+    AND salary < 40000;
 
 SELECT *
 FROM teachers
 WHERE last_name = 'Cole'
-      OR last_name = 'Bush';
+    OR last_name = 'Bush';
 
 SELECT *
 FROM teachers
 WHERE school = 'F.D. Roosevelt HS'
-      AND (salary < 38000 OR salary > 40000);
+    AND (
+        salary < 38000 
+        OR salary > 40000
+        );
 
 -- Listing 2-10: A SELECT statement including WHERE and ORDER BY
 
-SELECT first_name, last_name, school, hire_date, salary
+SELECT 
+    first_name
+    ,last_name
+    ,school
+    ,hire_date
+    ,salary
 FROM teachers
 WHERE school LIKE '%Roos%'
 ORDER BY hire_date DESC;
