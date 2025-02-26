5e0c782a240bf189c840f2352c5117ea637f5bca
Giacomo Allen <jagman.allen@gmail.com>
2025-02-26 15:19:15 +0000

    adding uncommitted files

---
+++ CC++/CompleteModernCPlusPlus/Section2[Basics]/first_project.cpp
@@ -0,0 +1,19 @@
+// This is a single line comment
+/*
+This is a multi-line comment
+*/
+
+// header file for console input / output
+// # statements are preprocessed
+#include <iostream>
+
+// main is the function invoked at runtime
+int main() {
+	// console input / output inside std namespace
+	// using insertion operator<<
+	// std::endl prints new line and flushes output buffer
+	std::cout << "Hello, world!" << std::endl;
+
+	// value returned to os
+	return 0;
+}
---
+++ CC++/CompleteModernCPlusPlus/Section2[Basics]/primitive_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    // intgral types
+    // scalar types
+    int i = 0;
+    char ch = 'a';
+    bool flag = true; // true or false
+
+    // floating point numbers
+    float f = 1.283f;
+    double d = 521.342;
+
+    // Vector type
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    return 0;
+}
