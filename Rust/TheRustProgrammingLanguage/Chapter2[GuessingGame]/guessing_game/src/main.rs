498b35b8904dc043b3d1f8db97d77a6e80033f91
Jag <jagman.allen@gmail.com>
2025-05-18 22:46:43 +0100

    Finished chapter 2 of Rust Programming. Created mini rust game.

---
+++ Rust/TheRustProgrammingLanguage/Chapter2[GuessingGame]/guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // start..=end inclusive range
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new(); // defined as mutable, vars immutable by default
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}"); // string interpolation, need ("{}", <expr>) to eval expresions
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        };
+    }
+}
